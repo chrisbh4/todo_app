01c5c788f7d503767eaee7b455646cad0d47f63a
Christian Brown <chrismbh4@gmail.com>
2023-02-20 18:24:08 -0800

    phx-change is pattern matching with event handlers & phx-submit is sumbitting with no errors but data does not show up in postbird

--- lib/todo_web/live/home_live.ex
+++ lib/todo_web/live/home_live.ex
@@ -1,11 +1,13 @@
 defmodule TodoWeb.HomeLive do
   use Phoenix.LiveView
+  use Phoenix.HTML
 
   import Todo.TodoItems
   alias Todo.Item
 
   def mount(_params, _url, socket) do
     changeset = Todo.Item.changeset(%Item{})
+
     socket = assign(socket, name: "Home Page", changeset: changeset)
     {:ok, socket}
   end
@@ -15,26 +17,34 @@ defmodule TodoWeb.HomeLive do
     <h1> <%= @name %></h1>
 
     <div>
-      <.form
-        for={@changeset}
-        phx-change="validate"
-        phx-submit="submit"
-        >
+      <.form let={f} for={@changeset} phx-change="form_change" phx-submit="submit">
 
-      <button type="submit">Submit </button>
+        <%= text_input f, :name , placeholder: "Name" %>
+        <%= textarea f, :description , placeholder: "Todo description" %>
+
+        <%= submit "Submit" %>
       </.form>
     </div>
+
+    
     """
   end
 
 
-  def handle_event("submit", %{"values" => values}, socket) do
-    new_item = Todo.TodoItems.create_item(values)
-
-    socket = assign(socket, new_item: new_item)
-
-    {:noreply , socket}
+  # def handle_event("form_change", %{"item" => %{"description" => values}}, socket) do
+  def handle_event("form_change", %{"item" => values}, socket) do
+    # IO.inspect(values)
+    {:noreply, socket}
   end
 
 
+  def handle_event("submit", %{"item" => values}, socket) do
+    IO.inspect(values)
+    new_item = Todo.TodoItems.create_item(values)
+    changeset = Todo.Item.changeset(%Item{})
+
+    socket = assign(socket, new_item: new_item, changeset: changeset)
+
+    {:noreply, socket}
+  end
 end
